95b914f02bf0b3bc72eed0862e6e300dafcedde0
Prathibha Ratnayake <prsthibha@gmail.com>
2023-06-15 20:48:07 +0530

    Add cors headers

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const port = 3000;
 
+app.use(cors())
 app.use(express.json())
 
 // Connect to MongoDB
