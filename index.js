9a5f8b05c8ecb6df20d60b8ab6a824768ee4c60a
Prathibha Ratnayake <prsthibha@gmail.com>
2023-06-15 20:33:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json())
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Failed to connect to MongoDB:', error);
+  });
+
+// Define a Product schema
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  description: String,
+  image: String,
+});
+
+// Create a Product model
+const Product = mongoose.model('Product', productSchema);
+
+// Set up routes
+app.get('/v1/products', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    console.error('Failed to fetch products:', error);
+    res.status(500).json({ error: 'Failed to fetch products' });
+  }
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
